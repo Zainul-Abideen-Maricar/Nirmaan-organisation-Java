9c9abb2fdf98eb864d7d76069f3f601c3dad9ed7
Zainul-Abideen-Maricar <zainulabideenmaricar@gmail.com>
2024-12-05 10:58:40 +0530

    Add files via upload
    Assesment

---
+++ Assesment.java
@@ -0,0 +1,136 @@
+package day8;
+
+import java.util.Scanner;
+
+public class Assesment {
+
+   int Assesment1(int age) {
+	return age;
+	  
+  }
+   
+  static int Assesment4(int num) {
+	   return num;
+   }
+ 
+
+   
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+//		Assesment assesment=new Assesment();
+//		
+//		
+//		
+//		
+//		System.out.print("Enter your Age: ");		
+//	    int age=sc.nextInt();
+//	    assesment.Assesment1(age);
+//    
+//	    if(age>=18) {
+//	    	System.out.println("you are eligible to vote.");
+//	    }
+//	    else {
+//	    	System.out.println("you are not eligible to vote.");
+//	    }
+//		
+		
+		 //Assesement 2
+		
+//		for(int i=1;i<=100;i++) {
+//			if(i%4==0) {
+//				System.out.println("Jack");
+//			}
+//			else {
+//				System.out.println(i);
+//			}
+//		}
+//		
+//	
+		
+	
+	  //Assesement 3
+		
+//	for(int i=1;i<70;i++) {
+//		if(i%2!=0) {
+//			continue;
+//		}else {
+//			System.out.println(i);
+//		}
+//	}
+		
+		//Assesement 4
+//		System.out.println("Enter number: ");
+//		int num=sc.nextInt();
+//		Assesment.Assesment4(num);
+//		
+//		
+//		if(num%2==0) {
+//			System.out.println(num +" is even number ");
+//		}
+//		else {
+//			System.out.println(num+ " is odd number");
+//		}
+//		
+		
+		//Assesment 5
+      
+//		System.out.println("Enter your name: ");
+//		
+//		String name=sc.nextLine();
+//		
+//		System.out.println(name.toLowerCase());
+//		System.out.println(name.toUpperCase());
+//		
+		
+		//Assement 6
+		
+//		System.out.println("Enter your mark: ");
+//		int mark=sc.nextInt();
+//		
+//		if(mark>=35) {
+//			System.out.println("pass");
+//		}
+//		else {
+//			System.out.println("fail");
+//		}
+		
+		//Assement 7
+//	    System.out.println("Enter your mark: ");
+//		int mark1=sc.nextInt();
+//		 System.out.println("Enter your mark: ");
+//	     int mark2=sc.nextInt();
+//		 System.out.println("Enter your mark: ");
+//		 int mark3=sc.nextInt();
+//		System.out.println("Enter your mark: ");
+//		int mark4=sc.nextInt();
+//		 System.out.println("Enter your mark: ");
+//		int mark5=sc.nextInt();
+//		System.out.println("Total:  ");
+//		int total=mark1+mark2+mark3+mark4+mark5;
+//		System.out.println(total);
+//		int average=total/5;
+//		System.out.println("Average:  ");
+//		System.out.println(average);
+//		
+		
+		
+		
+		
+		
+		
+		//Assesment 8
+		
+//		System.out.println("Enter year: ");
+//		int year=sc.nextInt();
+//		
+//		if(year%4==0) {
+//			System.out.println("It is an leap year");
+//		}
+//		else {
+//			System.out.println("It is not a leap year");
+//		}
+	}
+	
+}
